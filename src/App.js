69f8a7a83ad0d1c119f5f34de5be2289f17f04a8
unknown <ilhamsetyadji28@gmail.com>
2023-09-20 18:51:50 +0700

    last commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
 import "./App.css";
-import NavigationBar from "./components/NavigationBar";
-import "./style/LandingPage.css";
-import { Intro } from "./components/Intro";
-import { Trending } from "./components/Trending";
-import { SuperHero } from "./components/Superhero";
+import { useEffect, useState } from "react";
+import { getMovieList, searchMovie } from "./api";
 
-function App() {
+const App = () => {
+  const [movieList, setMovieList] = useState([]);
+
+  const search = async (e) => {
+    if (e.length > 3) {
+      const response = await searchMovie(e);
+      setMovieList(response.results);
+      console.log({ search: response });
+    }
+  };
+
+  useEffect(() => {
+    getMovieList().then((results) => {
+      setMovieList(results);
+    });
+  }, []);
+
+  const MoviePopular = () => {
+    return movieList.map((film, i) => {
+      return (
+        <div className="movie-wrapper" key={i}>
+          <div className="movie-title">
+            <h5>{film.title}</h5>
+          </div>
+          <img
+            src={`${process.env.REACT_APP_IMG_URL}/${film.poster_path}`}
+            alt="poster"
+            className="movie-image"
+          />
+          <div className="movie-date-rate">
+            <h5>Release: {film.release_date}</h5>
+            <h5 className="movie-rate">{film.vote_average}</h5>
+          </div>
+        </div>
+      );
+    });
+  };
+
   return (
-    <div>
-      {/* intro section */}
-      <div className="myBG">
-        <NavigationBar />
-        <Intro />
-      </div>
-      {/* end of intro */}
-      <div className="trending">
-        <Trending />
-      </div>
-      <div className="superhero">
-        <SuperHero/>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <h1 style={{marginTop:"1rem"}}>MOVIE MANIA</h1>
+        <input
+          placeholder="cari  film disini..."
+          className="movie-search"
+          onChange={({ target }) => search(target.value)}
+        />
+        <div className="movie-container">
+          <MoviePopular />
+        </div>
+      </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Trending.js
+++ src/components/Trending.js
@@ -5,31 +5,53 @@ import john from "../assets/john.jpeg";
 import lightyear from "../assets/lightyear.jpeg";
 import tokosebelah from "../assets/toko-sebelah.jpg";
 import morbius from "../assets/morbius.jpg";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export const Trending = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BASE_URL}/discover/movie`, {
+        params: {
+          api_key: process.env.REACT_APP_TMDB_KEY,
+        },
+      })
+      .then((response) => {
+        setMovies(response.data.results);
+      });
+  }, []);
   return (
     <div>
       <Container>
         <h1 className="text-white">TRENDING MOVIES</h1>
         <Row>
-          <Col md={4} className="movieWrapper" id="trending">
-            <Card className="movieImg">
-              <Image src={dune} alt="dune movies" className="images"/>
-              <div className="bg-dark">
-                <div className="m-1 p-2 text-white">
-                  <Card.Title className="text-center">DUNE</Card.Title>
-                  <Card.Text className="text-left">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </Card.Text>
-                  <Card.Text className="text-left">
-                    Last updated 3 mins ago
-                  </Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
+          {movies.map((result, index) => {
+            return (
+              <Col md={4} className="movieWrapper" id="trending" key={index}>
+                <Card className="movieImg">
+                  <Image
+                    src={`${process.env.REACT_APP_IMG_URL}/${result.poster_path}`}
+                    alt="image movies"
+                    className="images"
+                  />
+                  <div className="bg-dark">
+                    <div className="m-1 p-2 text-white">
+                      <Card.Title className="text-center">
+                        {result.title}
+                      </Card.Title>
+                      <Card.Text className="text-left">
+                        {result.overview}
+                      </Card.Text>
+                      <Card.Text className="text-left">
+                        {result.release_date}
+                      </Card.Text>
+                    </div>
+                  </div>
+                </Card>
+              </Col>
+            );
+          })}
         </Row>
       </Container>
     </div>
